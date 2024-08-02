aaa1005bb313dd63b790545270514e8f7b1e2d2c
tykkmm <tykhanew45@gmail.com>
2024-08-03 02:20:08 +0500

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,208 +1,226 @@
-import os
-import re 
-import sys
-import typing
-import asyncio 
-import logging 
-from database import db 
-from config import Config, temp
+import contextlib
 from pyrogram import Client, filters
-from pyrogram.raw.all import layer
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message 
-from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
-from pyrogram.errors import FloodWait
-from config import Config
-from translation import Translation
+from config import Config, temp
+from translation import Script
+from pyrogram.types import Message
+from database import db, mongodb_version
+from urllib.parse import urlparse
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from typing import Union, Optional, AsyncGenerator
+@Client.on_message(filters.command("start") & filters.private)
+async def start(bot: Client, cmd: Message):
+    txt = await cmd.reply("`Processing...`")
+    is_user = await is_user_exist(cmd.from_user.id)
+    if not is_user and LOG_CHANNEL: await bot.send_message(LOG_CHANNEL, f"#NewUser\n\nUser ID: `{cmd.from_user.id}`\nName: {cmd.from_user.mention}")
+    user = await get_user(cmd.from_user.id)
+    text = await translate(Script.START_MESSAGE, to_language=user['lang'])
+    return await txt.edit(text, reply_markup=Script.HELP_REPLY_MARKUP, disable_web_page_preview=True)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+@Client.on_message(filters.command("help") & filters.private)
+async def help(bot: Client, cmd):
+    txt = await cmd.reply("`Processing...`")
+    user = await get_user(cmd.from_user.id)
+    text = await translate(Script.HELP_MESSAGE, to_language=user['lang'])
+    return await txt.edit(text, reply_markup=Script.HOME_BUTTON_MARKUP)
 
-BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)]\[buttonurl:/{0,2}(.+?)(:same)?])")
-BOT_TOKEN_TEXT = "<b>1) create a bot using @BotFather\n2) Then you will get a message with bot token\n3) Forward that message to me</b>"
-SESSION_STRING_SIZE = 351
+@Client.on_message(filters.command("about") & filters.private)
+async def about(bot: Client, cmd):
+    if Script.ABOUT_MESSAGE:
+        txt = await cmd.reply("`Processing...`")
+        user = await get_user(cmd.from_user.id)
+        text = await translate(Script.ABOUT_MESSAGE, to_language=user['lang'])
+        return await txt.edit(text, reply_markup=Script.HOME_BUTTON_MARKUP)
 
-async def start_clone_bot(FwdBot, data=None):
-   await FwdBot.start()
-   #
-   async def iter_messages(
-      self, 
-      chat_id: Union[int, str], 
-      limit: int, 
-      offset: int = 0,
-      search: str = None,
-      filter: "types.TypeMessagesFilter" = None,
-      ) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
-   #
-   FwdBot.iter_messages = iter_messages
-   return FwdBot
+@Client.on_message(filters.command('addadmin') & filters.private)
+async def addadmin_handler(bot, m: Message):
+    if m.from_user.id != OWNER_ID:
+        return
 
-class CLIENT: 
-  def __init__(self):
-     self.api_id = Config.API_ID
-     self.api_hash = Config.API_HASH
+    config = await db.get_bot_stats()
+    admin_list = config["admins"]
+    tdl = ""
+    if admin_list:
+        for i in admin_list:
+            tdl += f"- `{i}`\n"
+    else:
+        tdl = "None\n"
+    if len(m.command) == 1:
+        return await m.reply(Script.ADD_ADMIN_TEXT.format(tdl))
+    try:
+        cmd = m.command
+        cmd.remove('addadmin')
+        if "remove_all" in cmd:
+            admin_list_new = []
+        elif "remove" in cmd:
+            cmd.remove('remove')
+            admin_list_cmd = [int(x) for x in "".join(cmd).strip().split(",")] 
+
+            for i in list(admin_list_cmd):
+                with contextlib.suppress(Exception):
+                    admin_list.remove(i)
+            admin_list_new = list(set(list(admin_list)))
+        else:
+            admin_list_cmd = [int(x) for x in "".join(cmd).strip().split(",")] 
+            admin_list_new = list(set(admin_list_cmd + list(admin_list)))
+
+        await db.update_stats({"admins": admin_list_new})
+        temp.ADMINS_LIST = admin_list_new
+        return await m.reply("Updated admin list successfully")
+    except Exception as e:
+        print(e)
+        return await m.reply("Some error updating admin list")
+
+@Client.on_message(filters.command('sleep') & filters.private)
+async def sleeptime_handler(bot, m: Message):
+    if m.from_user.id not in temp.ADMINS_LIST:
+        return
+    if len(m.command) != 2:
+        return await m.reply(f"`/sleep 5`\n\nCurrent Sleep time: {temp.SLEEP_TIME} seconds")
+
+    sleep_time = int(m.command[1])
+    await db.update_stats({'sleep_time': sleep_time})
+    temp.SLEEP_TIME = sleep_time
+    return await m.reply(f"Sleep time: {sleep_time} seconds Updated")
+
+
+@Client.on_message(filters.command('ban') & filters.private)
+async def banneduser_handler(bot, m: Message):
+    if m.from_user.id not in temp.ADMINS_LIST:
+        return
+    config = await db.get_bot_stats()
+    tdl = ""
+    if banned_user_list := config["banned_users"]:
+        for i in banned_user_list:
+            tdl += f"- `{i}`\n"
+    else:
+        tdl = "None\n"
+    if len(m.command) == 1:
+        return await m.reply(Script.BANNED_USERS_LIST.format(tdl))
+    try:
+        cmd = m.command
+        cmd.remove('ban')
+        if "remove_all" in cmd:
+            banned_user_list_new = []
+            await unbanalluser()
+
+        elif "remove" in cmd:
+            cmd.remove('remove')
+            banned_user_cmd = [int(x) for x in "".join(cmd).strip().split(",")] 
+
+            for i in list(banned_user_cmd):
+                with contextlib.suppress(Exception):
+                    await update_user_info(i, {'banned':False})
+                    banned_user_list.remove(i)
+
+            banned_user_list_new = list(set(list(banned_user_list)))
+        else:
+            banned_user_list_cmd = [int(x) for x in "".join(cmd).strip().split(",")] 
+            for user in banned_user_list_cmd:
+                await update_user_info(user, {'banned':True})
+
+            banned_user_list_new = list(set(banned_user_list_cmd + list(banned_user_list)))
+
+        await db.update_stats({"banned_users": banned_user_list_new})
+        temp.BANNED_USERS = banned_user_list_new
+        return await m.reply("Updated banned user list successfully")
+    except Exception as e:
+        print(e)
+        return await m.reply("Some error updating banned user list")
+
+@Client.on_message(filters.command('add_url') & filters.private)
+async def addurls_handler(bot, m: Message):
+
+    if m.from_user.id not in temp.ADMINS_LIST:
+        return
+
+    config = await db.filter_notify_url({})
+    tdl = ""
+    async for content in config:
+        tdl += f"- `{content['url']}` - {content['lang']}\n\n"
+        if len(tdl) > 4000 and len(m.command) == 1:
+            await m.reply(tdl)
+            tdl = ""
+
+    if len(m.command) == 1:
+        return await m.reply(tdl)
     
-  def client(self, data, user=None):
-     if user == None and data.get('is_bot') == False:
-        return Client("USERBOT", self.api_id, self.api_hash, session_string=data.get('session'))
-     elif user == True:
-        return Client("USERBOT", self.api_id, self.api_hash, session_string=data)
-     elif user != False:
-        data = data.get('token')
-     return Client("BOT", self.api_id, self.api_hash, bot_token=data, in_memory=True)
-  
-  async def add_bot(self, bot, message):
-     user_id = int(message.from_user.id)
-     msg = await bot.ask(chat_id=user_id, text=BOT_TOKEN_TEXT)
-     if msg.text=='/cancel':
-        return await msg.reply('<b>process cancelled !</b>')
-     elif not msg.forward_date:
-       return await msg.reply_text("<b>This is not a forward message</b>")
-     elif str(msg.forward_from.id) != "93372553":
-       return await msg.reply_text("<b>This message was not forward from bot father</b>")
-     bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', msg.text, re.IGNORECASE)
-     bot_token = bot_token[0] if bot_token else None
-     if not bot_token:
-       return await msg.reply_text("<b>There is no bot token in that message</b>")
-     try:
-       _client = await start_clone_bot(self.client(bot_token, False), True)
-     except Exception as e:
-       await msg.reply_text(f"<b>BOT ERROR:</b> `{e}`")
-     _bot = _client.me
-     details = {
-       'id': _bot.id,
-       'is_bot': True,
-       'user_id': user_id,
-       'name': _bot.first_name,
-       'token': bot_token,
-       'username': _bot.username 
-     }
-     await db.add_bot(details)
-     return True
-    
-  async def add_session(self, bot, message):
-     user_id = int(message.from_user.id)
-     text = "<b>⚠️ DISCLAIMER ⚠️</b>\n\n<code>you can use your session for forward message from private chat to another chat.\nPlease add your pyrogram session with your own risk. Their is a chance to ban your account. My developer is not responsible if your account may get banned.</code>"
-     await bot.send_message(user_id, text=text)
-     msg = await bot.ask(chat_id=user_id, text="<b>send your pyrogram session.\nget it from @mdsessiongenbot\n\n/cancel - cancel the process</b>")
-     if msg.text=='/cancel':
-        return await msg.reply('<b>process cancelled !</b>')
-     elif len(msg.text) < SESSION_STRING_SIZE:
-        return await msg.reply('<b>invalid session sring</b>')
-     try:
-       client = await start_clone_bot(self.client(msg.text, True), True)
-     except Exception as e:
-       await msg.reply_text(f"<b>USER BOT ERROR:</b> `{e}`")
-     user = client.me
-     details = {
-       'id': user.id,
-       'is_bot': False,
-       'user_id': user_id,
-       'name': user.first_name,
-       'session': msg.text,
-       'username': user.username
-     }
-     await db.add_bot(details)
-     return True
-    
-@Client.on_message(filters.private & filters.command('reset'))
-async def forward_tag(bot, m):
-   default = await db.get_configs("01")
-   temp.CONFIGS[m.from_user.id] = default
-   await db.update_configs(m.from_user.id, default)
-   await m.reply("successfully settings reseted ✔️")
+    try:
+        cmd = m.command
+        cmd.remove('add_url')
 
-@Client.on_message(filters.command('resetall') & filters.user(Config.BOT_OWNER_ID))
-async def resetall(bot, message):
-  users = await db.get_all_users()
-  sts = await message.reply("**processing**")
-  TEXT = "total: {}\nsuccess: {}\nfailed: {}\nexcept: {}"
-  total = success = failed = already = 0
-  ERRORS = []
-  async for user in users:
-      user_id = user['id']
-      default = await get_configs(user_id)
-      default['db_uri'] = None
-      total += 1
-      if total %10 == 0:
-         await sts.edit(TEXT.format(total, success, failed, already))
-      try: 
-         await db.update_configs(user_id, default)
-         success += 1
-      except Exception as e:
-         ERRORS.append(e)
-         failed += 1
-  if ERRORS:
-     await message.reply(ERRORS[:100])
-  await sts.edit("completed\n" + TEXT.format(total, success, failed, already))
-  
-async def get_configs(user_id):
-  #configs = temp.CONFIGS.get(user_id)
-  #if not configs:
-  configs = await db.get_configs(user_id)
-  #temp.CONFIGS[user_id] = configs 
-  return configs
-                          
-async def update_configs(user_id, key, value):
-  current = await db.get_configs(user_id)
-  if key in ['caption', 'duplicate', 'db_uri', 'forward_tag', 'protect', 'file_size', 'size_limit', 'extension', 'keywords', 'button']:
-     current[key] = value
-  else: 
-     current['filters'][key] = value
- # temp.CONFIGS[user_id] = value
-  await db.update_configs(user_id, current)
-    
-def parse_buttons(text, markup=True):
-    buttons = []
-    for match in BTN_URL_REGEX.finditer(text):
-        n_escapes = 0
-        to_check = match.start(1) - 1
-        while to_check > 0 and text[to_check] == "\\":
-            n_escapes += 1
-            to_check -= 1
+        if "remove_all" in cmd:
+            notify_urls_list_new = []
+            await db.deleteall_notify_url()
 
-        if n_escapes % 2 == 0:
-            if bool(match.group(4)) and buttons:
-                buttons[-1].append(InlineKeyboardButton(
-                    text=match.group(2),
-                    url=match.group(3).replace(" ", "")))
-            else:
-                buttons.append([InlineKeyboardButton(
-                    text=match.group(2),
-                    url=match.group(3).replace(" ", ""))])
-    if markup and buttons:
-       buttons = InlineKeyboardMarkup(buttons)
-    return buttons if buttons else None
+        elif "remove" in cmd:
+            cmd.remove('remove')
+            notify_urls_cmd = cmd[0]
+            await db.delete_notify_url(notify_urls_cmd)
+            
+        else:
+            langauge = cmd[0]
+            url = cmd[1]
+            domain = urlparse(url).netloc
+            await db.add_notify_url(url, langauge, domain)
+
+        notify_urls = await db.filter_notify_url({})
+        notify_urls_list_new = []
+        async for content in notify_urls:
+            notify_urls_list_new.append(content['api_url'])
+
+        temp.NOTIFY_URLS = notify_urls_list_new
+        return await m.reply("Updated urls list successfully")
+        
+    except Exception as e:
+        print(e)
+        return await m.reply("Some error updating urls list")
+
+@Client.on_message(filters.command('lang') & filters.private)
+async def lang_cmd_handler(bot, m: Message):
+    btn = [
+        [
+            InlineKeyboardButton(
+                text=f"{temp.LANG[lan]}", callback_data=f'changelang#{m.from_user.id}#{lan}#{temp.LANG[lan]}'
+            ),
+        ]
+        for lan in temp.LANG
+    ]
+    reply_markup = InlineKeyboardMarkup(btn)
+    user = await get_user(m.from_user.id)
+    await m.reply_text(f"Choose your language\nCurrent Language: {temp.LANG[user['lang']]}", reply_markup=reply_markup)
+
+@Client.on_message(filters.command('myplan') & filters.private)
+async def info_cmd_handler(bot, m: Message):
+    if len(m.command) == 1 and m.from_user.id in temp.ADMINS_LIST:
+        return await m.reply_text("`/myplan id`")
+    user_id = m.command[1] if m.from_user.id in temp.ADMINS_LIST else m.from_user.id
+    
+    btn = await get_user_info_button(user_id)
+    text = await get_user_info_text(user_id)
+    await m.reply(text, reply_markup=InlineKeyboardMarkup(btn) if m.from_user.id in temp.ADMINS_LIST else None)
+
+@Client.on_message(filters.command('premium_users') & filters.private)
+async def premium_users_cmd(bot: Client, m: Message):
+    if m.from_user.id not in temp.ADMINS_LIST:
+        return
+
+    premium_users = await filter_users({"has_access":True, "banned":False})
+    text = "List of premium users\n\n"
+    bin_text = ""
+    async for user in premium_users:
+        if await is_user_verified(user["user_id"]) or user["has_access"] == False:
+            tg_user = await bot.get_users(user["user_id"])
+            bin_text += "- `{user_id}` {user_link}\n".format(user_id=user["user_id"], user_link=tg_user.mention)
+    bin_text = bin_text or "None"
+    await m.reply(text+bin_text)
+
+@Client.on_message(filters.command('serial_lang') & filters.private)
+async def serial_lang_cmd(bot, m: Message):
+    if not IS_USER_ALLOWED_TO_CHANGE_LANGUAGE:
+        return
+    text, btn = await get_serial_language(m.from_user.id)
+    await m.reply(text, reply_markup=InlineKeyboardMarkup(btn))
+
+@Client.on_message(filters.command('id') & filters.private)
+async def id_cmd(bot, m: Message):
+    return await m.reply(f"Chat: {m.from_user.first_name}\nID: `{m.from_user.id}`")
